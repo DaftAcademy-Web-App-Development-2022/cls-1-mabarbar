ba05dc69b8c65ed29c0288ba9102b77b6268c504
mabarbar <mateusz.barzenc00@gmail.com>
2022-11-22 17:35:32 +0100

    user and sidebar

--- src/components/Sidebar/Sidebar.component.tsx
+++ src/components/Sidebar/Sidebar.component.tsx
@@ -1,18 +1,31 @@
-import React from 'react'
+import React from "react";
 
-import styles from "./SideBar.module.css"
+import styles from "./SideBar.module.css";
+import { User, Form } from "../index";
+
+import userData from "~/data/userData.json";
 
 const SideBar = () => {
+  
+  const handleLogout = () => {
+    console.log("logout");
+  };
+
   return (
-    <div className='styles.root'>
-      <div className='styles.top'>
-        top
+    <div className={styles.root}>
+      <div className={styles.top}>
+        <User
+          name={userData.name}
+          email={userData.email}
+          image={userData.url}
+          logout={handleLogout}
+        />
       </div>
-      <div className='styles.center'>
-        center
+      <div className={styles.center}>
+        <Form />
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default SideBar;
